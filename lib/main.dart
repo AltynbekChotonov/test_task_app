fa5376e07d6150d70de0ba46b34cbe5cfbf3951d
AltynbekChotonov <dtvkanalnews@gmail.com>
2023-04-27 14:23:29 +0600

    test tapshyrma

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+//? CodeWithFlexz on Instagram
+
+//* AmirBayat0 on Github
+//! Programming with Flexz on Youtube
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:unsplash_api_test/view/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Api Test',
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
